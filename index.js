0d5151b8b5c53ad5b2d9bdce26ebddd194f98db4
zannatin-tazri <zannatintazrizan@gmail.com>
2023-05-27 02:46:38 +0600

    horse and dinosaur data loaded

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const port =process.env.PORT|| 5000;
 
 const teddyBear=require('./data/tedy.json');
 const horse=require('./data/horse.json');
+const dinosaur=require('./data/dinosaur.json');
 
+app.use(cors());
 app.use(cors());
 app.use(cors());
 app.get('/',(req,res)=>{
-
     res.send('hello');
-
 });
 
 app.get('/teddyBear',(req,res)=>{
@@ -19,6 +19,9 @@ app.get('/teddyBear',(req,res)=>{
 })
 app.get('/horse',(req,res)=>{
     res.send(horse);
+})
+app.get('/dinosaur',(req,res)=>{
+    res.send(dinosaur);
 })
 
 app.listen(port,()=>{
