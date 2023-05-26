d66c10e768e66d1db09115396d5b142378106978
zannatin-tazri <zannatintazrizan@gmail.com>
2023-05-27 01:59:20 +0600

    horse data added

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors=require('cors');
 const port =process.env.PORT|| 5000;
 
 const teddyBear=require('./data/tedy.json');
+const horse=require('./data/horse.json');
 
+app.use(cors());
 app.use(cors());
 app.get('/',(req,res)=>{
 
@@ -14,6 +16,9 @@ app.get('/',(req,res)=>{
 
 app.get('/teddyBear',(req,res)=>{
     res.send(teddyBear);
+})
+app.get('/horse',(req,res)=>{
+    res.send(horse);
 })
 
 app.listen(port,()=>{
